eceee80530d6a9d5415faab3f1b054df47e568e3
Eric Mauldin <emauldin84@gmail.com>
2019-06-14 11:13:32 -0400

    ejects app and configures and uses css modules for styling

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,11 @@
 import React from 'react'
 // import Radium from 'radium'
-import './Person.css'
+import classes from './Person.css'
 
 const Person = (props) => {
     
     return (
-    <div className='Person'>
+    <div className={classes.Person}>
         <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
         <p>{props.children}</p>
         <input type="text" value={props.name} onChange={props.changed}/>
