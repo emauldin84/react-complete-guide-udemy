ed8601725bd36f31ee796df6092e9375a6f735a2
Eric Mauldin <emauldin84@gmail.com>
2019-06-14 11:57:08 -0400

    shows creates and uses ErrorBoundary component to catch errors on the Persons component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,6 +4,11 @@ import classes from './Person.css'
 
 const Person = (props) => {
     
+    const rnd = Math.random();
+    if(rnd > .7) {
+        throw new Error('Something went wrong')
+    }
+    
     return (
     <div className={classes.Person}>
         <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
